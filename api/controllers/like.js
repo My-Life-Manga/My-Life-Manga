6ebcff2f78f6781ab7169631e688c2927ddcd17a
Andrew-Neely-82 <andrew.neely82@gmail.com>
2023-03-08 17:41:57 -0600

    likes are working as intended

--- api/controllers/like.js
+++ api/controllers/like.js
@@ -17,18 +17,16 @@ export const addLike = (req, res) => {
   jwt.verify(token, "secretkey", (err, userInfo) => {
     if (err) return res.status(403).json("Token is not valid!");
 
-    const q = "INSERT INTO likes (`userId`,`postId`) VALUES (?)";
-    const values = [
-      userInfo.id,
-      req.body.postId
-    ];
+    const q = "INSERT INTO likes (`userId`,`postId`) VALUES (?,?)";
+    const values = [userInfo.id, req.body.postId];
 
-    db.query(q, [values], (err, data) => {
+    db.query(q, values, (err, data) => {
       if (err) return res.status(500).json(err);
       return res.status(200).json("Post has been liked.");
     });
   });
 };
+
 
 export const deleteLike = (req, res) => {
 
