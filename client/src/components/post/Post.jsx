47cbefb27aac2175d9f941cdaf68f5ff4253db1b
Andrew-Neely-82 <andrew.neely82@gmail.com>
2023-03-09 13:02:41 -0600

    restructured icons for posts

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,25 +1,20 @@
-import "./post.scss";
-import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
-import FavoriteOutlinedIcon from "@mui/icons-material/FavoriteOutlined";
-import TextsmsOutlinedIcon from "@mui/icons-material/TextsmsOutlined";
-import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
-import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
-import { Link } from "react-router-dom";
+import {useState , useContext} from "react";
+import icons from './icons'
+import {Link} from "react-router-dom";
 import Comments from "../comments/Comments";
-import { useState } from "react";
 import moment from "moment";
-import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
-import { makeRequest } from "../../axios";
-import { useContext } from "react";
-import { AuthContext } from "../../context/authContext";
+import {useQuery, useQueryClient, useMutation} from "@tanstack/react-query";
+import {makeRequest} from "../../axios";
+import {AuthContext} from "../../context/authContext";
+import "./post.scss";
 
-const Post = ({ post }) => {
+const Post = ({post}) => {
   const [commentOpen, setCommentOpen] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
 
-  const { currentUser } = useContext(AuthContext);
+  const {currentUser} = useContext(AuthContext);
 
-  const { isLoading, error, data } = useQuery(["likes", post.id], () =>
+  const {isLoading, error, data} = useQuery(["likes", post.id], () =>
     makeRequest.get("/likes?postId=" + post.id).then((res) => {
       return res.data;
     })
@@ -30,7 +25,7 @@ const Post = ({ post }) => {
   const mutation = useMutation(
     (liked) => {
       if (liked) return makeRequest.delete("/likes?postId=" + post.id);
-      return makeRequest.post("/likes", { postId: post.id });
+      return makeRequest.post("/likes", {postId: post.id});
     },
     {
       onSuccess: () => {
@@ -64,50 +59,37 @@ const Post = ({ post }) => {
       <div className="container">
         <div className="user">
           <div className="userInfo">
-            <img src={"/upload/"+post.profilePic} alt="" />
+            <img src={"/upload/" + post.profilePic} alt=""/>
             <div className="details">
-              <Link
-                to={`/profile/${post.userId}`}
-                style={{ textDecoration: "none", color: "inherit" }}
-              >
+              <Link to={`/profile/${post.userId}`} style={{textDecoration: "none", color: "inherit"}}>
                 <span className="name">{post.name}</span>
               </Link>
               <span className="date">{moment(post.createdAt).fromNow()}</span>
             </div>
           </div>
-          <MoreHorizIcon onClick={() => setMenuOpen(!menuOpen)} />
+          <icons.MoreHorizIcon onClick={() => setMenuOpen(!menuOpen)}/>
           {menuOpen && post.userId === currentUser.id && (
             <button onClick={handleDelete}>delete</button>
           )}
         </div>
         <div className="content">
           <p>{post.desc}</p>
-          <img src={"/upload/" + post.img} alt="" />
+          <img src={"/upload/" + post.img} alt=""/>
         </div>
         <div className="info">
           <div className="item">
-            {isLoading ? (
-              "loading"
-            ) : data.includes(currentUser.id) ? (
-              <FavoriteOutlinedIcon
-                style={{ color: "red" }}
+            {isLoading ? ("loading") : data.includes(currentUser.id) ? (
+              <icons.FavoriteOutlinedIcon
+                style={{color: "red"}}
                 onClick={handleLike}
               />
-            ) : (
-              <FavoriteBorderOutlinedIcon onClick={handleLike} />
-            )}
+            ) : (<icons.FavoriteBorderOutlinedIcon onClick={handleLike}/>)}
             {data?.length} Likes
           </div>
-          <div className="item" onClick={() => setCommentOpen(!commentOpen)}>
-            <TextsmsOutlinedIcon />
-            See Comments
-          </div>
-          <div className="item">
-            <ShareOutlinedIcon />
-            Share
-          </div>
+          <div className="item" onClick={() => setCommentOpen(!commentOpen)}><icons.TextsmsOutlinedIcon/>See Comments</div>
+          <div className="item"><icons.ShareOutlinedIcon/>Share</div>
         </div>
-        {commentOpen && <Comments postId={post.id} />}
+        {commentOpen && <Comments postId={post.id}/>}
       </div>
     </div>
   );
