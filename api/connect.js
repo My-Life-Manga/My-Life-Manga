c6411703f01811c826fafb5d642ae301c606c09b
Crystal <crystal.ochoa03@gmail.com>
2023-03-10 23:49:17 +0100

    RDS

--- api/connect.js
+++ api/connect.js
@@ -1,8 +1,8 @@
 import mysql from "mysql"
 
 export const db = mysql.createConnection({
-  host:"127.0.0.1",
-  user:"root",
-  password:"codeup",
+  host:"my-manga-db.cnwidfzee54g.us-east-1.rds.amazonaws.com",
+  user:"admin",
+  password:"Germany0307",
   database:"manga_db"
-})
+});
