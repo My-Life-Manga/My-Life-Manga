269395b03ad7880d2fd87ea8e87759f2e4205cca
Crystal <crystal.ochoa03@gmail.com>
2023-03-13 23:10:11 +0100

    crystals-update

--- client/src/components/comments/Comments.jsx
+++ client/src/components/comments/Comments.jsx
@@ -26,8 +26,7 @@ const Comments = ({postId}) => {
         // Invalidate and refetch
         queryClient.invalidateQueries(["comments"]);
       },
-    }
-  );
+    });
 
   const handleClick = async (e) => {
     e.preventDefault();
@@ -39,30 +38,21 @@ const Comments = ({postId}) => {
     <div className="comments">
       <div className="write">
         <img src={"/upload/" + currentUser.profilePic} alt=""/>
-        <input
-          type="text"
-          placeholder="write a comment"
-          value={desc}
-          onChange={(e) => setDesc(e.target.value)}
-        />
+        <input type="text" placeholder="write a comment" value={desc} onChange={(e) => setDesc(e.target.value)}/>
         <button onClick={handleClick}>Send</button>
       </div>
-      {error``
-        ? "Something went wrong"
-        : isLoading
-          ? "loading"
-          : data.map((comment) => (
-            <div className="comment">
-              <img src={"/upload/" + comment.profilePic} alt=""/>
-              <div className="info">
-                <span>{comment.name}</span>
-                <p>{comment.desc}</p>
-              </div>
-              <span className="date">
-                {moment(comment.createdAt).fromNow()}
-              </span>
-            </div>
-          ))}
+      {error ? "Something went wrong" : isLoading ? "loading" : data.map((comment) => (
+        <div className="comment">
+          <img src={"/upload/" + comment.profilePic} alt=""/>
+          <div className="info">
+            <span>{comment.name}</span>
+            <p>{comment.desc}</p>
+          </div>
+          <span className="date">
+            {moment(comment.createdAt).fromNow()}
+          </span>
+        </div>
+      ))}
     </div>
   );
 };
